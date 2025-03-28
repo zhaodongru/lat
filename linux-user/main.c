4628add610e0556c68eb8132f96128a3b9e858a5
Wenqiang Wei <weiwenqiang@mail.ustc.edu.cn>
2025-03-28 16:52:43 +0800

    LATX, fix: Adjustable initial address for guest address.
    Wine will reserve some addresses for special applications, and it is
    best not to directly use these addresses in our target_mmap(). This patch
    adds an environment variable to control the initial address of
    target_mmap.
    For example: export LATX_MMAP_START=0x90000000.
    
    Signed-off-by: Wenqiang Wei <weiwenqiang@mail.ustc.edu.cn>

--- linux-user/main.c
+++ linux-user/main.c
@@ -676,6 +676,11 @@ static void handle_arg_latx_anonym(const char *arg)
 #endif
 }
 
+static void handle_arg_mmap_start(const char *arg)
+{
+    mmap_next_start = strtol(arg, NULL, 0);
+}
+
 static void handle_arg_latx_mem_test(const char *arg)
 {
     option_mem_test = strtol(arg, NULL, 0);
@@ -802,6 +807,8 @@ static const struct qemu_argument arg_table[] = {
 #endif
     {"latx-anonym",    "LATX_ANONYM",     true,  handle_arg_latx_anonym,
     "",           "anonymize latx for guest"},
+    {"latx-mmap_start",    "LATX_MMAP_START",     true,  handle_arg_mmap_start,
+    "",           "adjust the initial address of mmap"},
 #endif
 #if defined(CONFIG_LATX_DEBUG) || defined(CONFIG_DEBUG_TCG)
 #ifdef CONFIG_LATX
